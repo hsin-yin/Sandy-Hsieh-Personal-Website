0a696dc1b4ba248452c522c38ce84c8fd5e2ff99
hsin-yin <hyh50276@gmail.com>
2024-01-15 20:46:06 +0800

    update server

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { google } = require("googleapis");
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 
 app.use(cors());
 app.use(bodyParser.json());
