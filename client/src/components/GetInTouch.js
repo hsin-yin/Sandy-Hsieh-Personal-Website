7e9ac10ac7364d7a69d46ea92ce95c6b98970a45
hsin-yin <hyh50276@gmail.com>
2024-01-15 22:31:01 +0800

    change url

--- client/src/components/GetInTouch.js
+++ client/src/components/GetInTouch.js
@@ -36,7 +36,7 @@ const GetInTouch = () => {
         }
         try {
             setLoading(true);
-            const response = await axios.post('https://sandy-hsieh.up.railway.app/submit', formData, {
+            const response = await axios.post('https://sandy-hsieh-server.up.railway.app/submit', formData, {
                 headers: {
                     'Content-Type': 'application/json'
                 }
